8644f706dfef7db3d9c1b22ef618c66dcfdf527b
LynnAmsbury <Lynn8542@gmail.com>
2020-05-21 13:30:13 -0600

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,35 @@
 # Add your code here
+require 'pry'
+
+class Dog
+    attr_reader :name
+    @@all = []
+
+    def initialize (name)
+    @name = name
+    self.save
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        all.clear
+    end
+
+    def self.print_all
+        dog_names = all.map do |dog|
+            dog.name.to_s
+        end
+        puts dog_names
+    end
+
+    def save
+        # @@all << self
+        self.class.all << self
+    end
+
+    
+
+end
